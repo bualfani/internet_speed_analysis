4dc7b317c06fb1a86d73434992861c427729f824
bahati <bulozealfani@gmail.com>
2023-03-30 10:26:40 -0500

    added empty list to store the data

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import time
 
 # create speedtest client
 st = speedtest.Speedtest()
+
+# empty list to store data
+times = []
+upload_speed = []
+download_speed = []
+
+# number of data points to collect
+num_points = 10
