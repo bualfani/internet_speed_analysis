013b04aebcabb16ea303261e6b0675233053ec9d
bahati <bulozealfani@gmail.com>
2023-03-30 10:31:02 -0500

    ran speed test and appended data to list

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ download_speed = []
 
 # number of data points to collect
 num_points = 10
+
+# loop to collect data points
+for i in range(num_points):
+    download_speed = st.download()/ 1_000_000
+    upload_speed = st.upload() / 1_000_000
+    current_time = time.strftime("%H:%M:%S", time.localtime())
+    times.append(current_time)
+    download_speed.append(download_speed)
+    upload_speed.append(upload_speed)
+    # pause for 5 seconds between datat points
+    time.sleep(5)
